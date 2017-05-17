e201adab07e36c58e3fe97454ec3c3b52d65a44d
Quang Hoang <quangh@fenestrae.com>
2017-05-17 11:36:57 +0200

    17-5

--- Lets-code/347. Top K Frequent Elements - Version 2 - heap.py
+++ Lets-code/347. Top K Frequent Elements - Version 2 - heap.py
@@ -15,12 +15,15 @@ class Solution(object):
             c[i]+=1
         heap = []
 
+        list_c = []
         for key, value in c.iteritems():
-            heapq.heappush(heap, (-value,key))
+            #heapq.heappush(heap, (-value,key))
+            list_c.append((-value, key))
 
+        heapq.heapify(list_c)
         res = []
         while k>0:
-            res.append(heapq.heappop(heap)[1])
+            res.append(heapq.heappop(list_c)[1])
             k-=1
 
         return res
@@ -28,6 +31,6 @@ class Solution(object):
 
 
 
-#s = Solution().topKFrequent([1,4,1,1,2,7,5,2,4],3)
-s = Solution().topKFrequent([1,2],2)
+s = Solution().topKFrequent([1,4,1,1,2,7,5,2,4],3)
+#s = Solution().topKFrequent([1,2],2)
 print s
